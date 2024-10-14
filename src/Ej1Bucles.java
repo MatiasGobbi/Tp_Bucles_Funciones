f54a51336f2bebce79dc0dfbf5af68109ad5dbb6
MatiasGobbi <migobbi555@gmail.com>
2024-10-14 16:45:29 -0300

    Se modificó Ej1Bucles.java Se subió Ej2Bucles.java

--- src/Ej1Bucles.java
+++ src/Ej1Bucles.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+/*
+Ejercicio 1: Cálculo del tiempo dedicado a cada actividad
+Un alumno quiere medir el tiempo total que dedica a sus actividades académicas y personales durante el día.
+Instrucciones:
+Pide al usuario que ingrese las horas dedicadas a cada actividad: estudiar, hacer ejercicios, leer, y tiempo libre.
+Usa un bucle para registrar las horas y luego calcula el tiempo total dedicado.
+*/
+
+import java.util.Scanner;
 
 public class Ej1Bucles {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] actividades = {"Estudiar", "Hacer ejercicios", "Leer", "Tiempo libre"};
+        int totalHoras = 0;
+
+        // Bucle para pedir horas por cada actividad
+        for (String actividad : actividades) {
+            System.out.print("¿Cuántas horas dedicas a " + actividad + "? ");
+            int horas = scanner.nextInt();
+            totalHoras += horas;
+        }
+        System.out.println("El tiempo total dedicado a tus actividades es: " + totalHoras + " horas.");
+    }
 }
---
+++ src/Ej2Bucles.java
@@ -0,0 +1,29 @@
+/*
+Una empresa desea calcular el salario semanal de sus empleados basándose en las horas trabajadas y una tarifa fija por hora.
+Instrucciones:
+Pide al usuario ingresar la cantidad de empleados.
+Para cada empleado, pide ingresar las horas trabajadas.
+Usa un bucle para calcular el salario de cada empleado (suponiendo una tarifa fija de $15 por hora).
+*/
+
+import java.util.Scanner;
+
+public class Ej2Bucles {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de empleados: ");
+        int cant_empleados = sc.nextInt();
+
+        // Tarifa fija por hora
+        final int tarifa_por_hora = 15;
+
+        // Bucle para calcular el salario de cada empleado
+        for (int i = 1; i <= cant_empleados; i++) {
+            System.out.println("Ingrese las horas trabajadas por el empleado " + i + ": ");
+            int horas_trabajadas = sc.nextInt();
+            int salario = horas_trabajadas * tarifa_por_hora;
+            System.out.println("El salario correspondiente al empleado " + i + " es: $" + salario);
+        }
+    }
+}
